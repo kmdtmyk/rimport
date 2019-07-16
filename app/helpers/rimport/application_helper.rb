a42860daeb392dbc8df09ada769d22678c69b125
kmdtmyk <winback_banderas@yahoo.co.jp>
2019-07-16 15:28:22 +0900

    Refactoring

--- app/helpers/rimport/application_helper.rb
+++ app/helpers/rimport/application_helper.rb
@@ -7,5 +7,15 @@ module Rimport
       datetime.strftime('%Y/%m/%d %H:%M:%S') if datetime.present?
     end
 
+    def table_names
+      internal_tables = %w(
+        schema_migrations
+        ar_internal_metadata
+        active_storage_attachments
+        active_storage_blobs
+      )
+      ActiveRecord::Base.connection.tables - internal_tables
+    end
+
   end
 end
